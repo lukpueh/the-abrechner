10b41d423dc9408fd5bbc1dd1d4ca5d48699cc75
Lukas P <luk.puehringer@gmail.com>
2015-05-26 16:43:02 +0200

    add form validation, add delete confirmation, add 404 handling, add niceness

--- the-abrechner.js
+++ the-abrechner.js
@@ -3,39 +3,80 @@ Persons = new Mongo.Collection("persons");
 Items = new Mongo.Collection("items");
 
 Router.configure({
-    layoutTemplate: 'main'
+    layoutTemplate: 'main',
+    notFoundTemplate: 'notFound'
 });
 Router.map(function(){
-    this.route('home', {path: '/'} );
+    this.route('home', {
+        path: '/',
+    });
     this.route('abrechner', {
         path: '/:link',
-        data: function() {
+        waitOn: function() {
             var link = this.params.link;
             Session.set("link", link);
-            var abrechnung = Abrechnungs.findOne({"link": link});
-            return abrechnung;
+            return [
+                Meteor.subscribe("abrechnungs", link),
+                Meteor.subscribe("items", link),
+                Meteor.subscribe("persons", link)];
+        },
+        data: function() {
+            return Abrechnungs.findOne({"link": this.params.link});
         }
     });
 });
 
+Router.onBeforeAction('dataNotFound');
+
 if (Meteor.isClient) {
 
-    Tracker.autorun(function () {
-        var link = Session.get("link");
-        if (typeof link != 'undefined' && link != null) {
-            Meteor.subscribe("abrechnungs", link);
-            Meteor.subscribe("items", link);
-            Meteor.subscribe("persons", link);
-        }
+    //Work around for template rendering animation
+    Template.message.onRendered(function(){
+        $(this.firstNode).fadeIn();
+    });
+
+    function showMessage(text, where){
+        text += "This will definitely lead to distrust and bloodshed.";
+        Blaze.renderWithData(Template.message, 
+                            {"text" : text}, 
+                            where, //Append to
+                            $(where).children(":first-child").get(0)); //but before first child
+    }
+
+    Template.main.onRendered(function(){
+        /***********************************************
+        * Encrypt Email script- Please keep notice intact
+        * Tool URL: http://www.dynamicdrive.com/emailriddler/
+        * **********************************************/
+
+        var emailriddlerarray=[108,117,107,46,112,117,101,104,114,105,110,103,101,114,64,103,109,97,105,108,46,99,111,109];
+        var encryptedemail_id42=''; //variable to contain encrypted email 
+        for (var i=0; i<emailriddlerarray.length; i++)
+            encryptedemail_id42+=String.fromCharCode(emailriddlerarray[i]);
+            $("#email").attr("href", "mailto:" + encryptedemail_id42);
     });
 
     Template.home.events({
         "submit .new-abrechner": function(event) {
+            var form = event.target;
+            $(form).find(".message").remove();
+
             var title = event.target.title.value;
             var description = event.target.description.value;
-            Meteor.call("addAbrechner", title, description, function(err, link){
-                Router.go('abrechner', {"link": link});
-            });
+            var message = "";
+            if (title.length < 1)
+                message += "What, no title? ";
+            if (title.length > 20)
+                message += "A title with more than 20 characters is silly. ";
+            if (description.length > 255)
+                message += "A description with more than 255 characters is silly. ";
+
+            if (message)
+                showMessage(message, form);
+            else
+                Meteor.call("addAbrechner", title, description, function(err, link){
+                    Router.go('abrechner', {"link": link});
+                });
             return false;
         }
     });
@@ -57,10 +98,147 @@ if (Meteor.isClient) {
         },
         personNameById: function(personId) {
             var person = Persons.findOne(personId);
-            if (person)
-               return person.name;
-            return "Nobody";
+            return person.name;
+        }
+    });
+
+    Template.abrechner.events({
+        // Add new Person
+        "submit .new-person": function (event) {
+            var form = event.target;
+            $(form).find(".message").remove();
+
+            var link = Session.get("link");
+            var name = event.target.person.value;
+
+            var message = "";
+            if (name.length < 1)
+                message += "What, your person has no name? ";
+            if (name.length > 20)
+                message += "Persons with names with more than 20 characters are silly. ";
+            if (Persons.find({"name": name}).count() > 0)
+                message += "There is already someone with that name. ";
+
+            if (message){
+                showMessage(message, form);
+            } else {
+                Meteor.call("addPerson", link, name);
+                event.target.person.value = "";
+            }
+
+            
+            return false;
         },
+        "click .delete-person": function () {
+            var form = $("form.new-person").get(0);
+            $(form).find(".message").remove();
+
+            var link = Session.get("link");
+            var personId = this._id;
+            var message = "";
+            if (Items.find({"paidBy" : personId}).count() > 0)
+                message = "Hey, this person has already paid for something. ";
+
+            if (message)
+                showMessage(message, form)
+            else
+                Meteor.call("removePerson", link, personId);
+            return false;
+        },
+        // Add new item
+        "submit .new-item": function (event) {
+            var form = event.target;
+            $(form).find(".message").remove();
+
+            var link = Session.get("link");
+            var title = event.target.item.value;
+            var amount = event.target.amount.value;
+            var personId = event.target.person.value;
+
+            var message = "";
+            if (title.length < 1)
+                message += "What, no title? ";
+            if (title.length > 20)
+                message += "Item titles with more than 20 characters are silly. ";
+            if (!$.isNumeric(amount))
+                message += "You specified a silly number for the costs! ";
+            if (Persons.find(personId).count() < 1)
+                message += "Who paid for the item? ";
+
+            if (message){
+                showMessage(message, form);
+            } else {
+                amount = parseFloat(amount)
+                Meteor.call("addItem", link, title, amount, personId);
+                event.target.item.value = "";
+                event.target.amount.value = "";
+            }
+            return false;
+        },
+        "click .delete-item": function () {
+            var link = Session.get("link");
+            var itemId = this._id;
+            Meteor.call("removeItem", link, itemId);
+        },
+
+        "change .pays-share": function(event) {
+            var link = Session.get("link");
+            var itemId = $(event.target).data("item-id");
+            var personId = this.person;
+            var checked = event.target.checked;
+
+            Meteor.call("updateShare",link, itemId, personId, checked);
+        },
+        "click .delete-abrechnung": function(event) {
+            var link = Session.get("link");
+            var abrechnungsId = this._id;
+            Meteor.call("removeAbrechnung", link, abrechnungsId);
+            Router.go('home');
+        },
+
+        "click .mark-url": function(event) {
+            $("#url").focus();
+            $("#url").select();
+        }
+    });
+
+    Template.breakdown.onRendered(function(){
+        //Datatables
+        $('#breakdownTable').DataTable({
+            "ordering": true,
+            "paging": false,
+            "info" : false,
+            "bFilter": false
+        });
+
+        // Draw pie if container rendered for the first time
+        var paperH = 300, paperW = 300
+        var paper = Raphael("raphael-container", paperW, paperH);
+        Tracker.autorun(function(){
+            var items = Items.find().fetch();
+            paper.clear();
+            var totalAmounts = [];
+            var totalItems = [];
+            items.forEach(function(item){
+                totalAmounts.push(item.amount);
+                totalItems.push(item.title + " - € " + item.amount);
+            });
+            var chart = paper.piechart(150, 150, 100, totalAmounts, {
+                legend: totalItems, 
+                legendpos: "south", 
+            });  
+            var legendH = $("#raphael-container svg text").height() * items.length;
+            paper.setSize(paperW, paperH + legendH)
+        });
+    });
+
+    Template.breakdown.helpers({
+        diffClass: function(diff) {
+            if (diff < 0)
+                return "text-danger";
+            else
+                return "text-success";
+        }, 
         sum: function(){
             var sum = 0;
             Items.find().forEach(function(item){
@@ -68,11 +246,18 @@ if (Meteor.isClient) {
             });
             return sum;
         },
+        fairShare: function() {
+            var sum = 0;
+            Items.find().forEach(function(item){
+                sum += item.amount;
+            });
+            return sum / Persons.find().count()
+        },
         /*
          * Return an array with all involved persons specifying 
          * who paid what and who has to pay what.
          */
-        breakDown: function() {
+        calcs: function() {
             //Create an associative array for all involved persons, personId is Key
             var persons = new Array(); 
             Persons.find().forEach(function(person) {
@@ -109,84 +294,7 @@ if (Meteor.isClient) {
              });
             return result;
         },
-
-    });
-
-    Template.abrechner.events({
-        // Add new Person
-        "submit .new-person": function (event) {
-            var link = Session.get("link");
-            var name = event.target.person.value;
-            Meteor.call("addPerson", link, name);
-            
-            event.target.person.value = "";
-            return false;
-        },
-        "click .delete-person": function () {
-            var link = Session.get("link");
-            var personId = this._id;
-            if (Items.find({"paidBy" : personId}).count() > 0){
-                alert("Hey he has already paid");
-                return false;
-            }
-            Meteor.call("removePerson", link, personId);
-        },
-        // Add new item
-        "submit .new-item": function (event) {
-            var link = Session.get("link");
-            var title = event.target.item.value;
-            var amount = parseInt(event.target.amount.value);
-            var personId = event.target.person.value;
-
-            Meteor.call("addItem", link, title, amount, personId);
-            
-            event.target.item.value = "";
-            event.target.amount.value = "";
-            return false;
-        },
-        "click .delete-item": function () {
-            var link = Session.get("link");
-            var itemId = this._id;
-            Meteor.call("removeItem", link, itemId);
-        },
-
-        "change .pays-share": function(event) {
-            var link = Session.get("link");
-            var itemId = $(event.target).data("item-id");
-            var personId = this.person;
-            var checked = event.target.checked;
-
-            Meteor.call("updateShare",link, itemId, personId, checked);
-        },
-        "click .delete-abrechnung": function(event) {
-            var link = Session.get("link");
-            var abrechnungsId = this._id;
-            Meteor.call("removeAbrechnung", link, abrechnungsId);
-            Router.go('home');
-        }
-    });
-
-    // Draw pie if container rendered for the first time
-    Template.raphael.onRendered(function(){
-        var paperH = 300, paperW = 300
-        var paper = Raphael("raphael-container", paperW, paperH);
-        Tracker.autorun(function(){
-            var items = Items.find().fetch();
-            paper.clear();
-            var totalAmounts = [];
-            var totalItems = [];
-            items.forEach(function(item){
-                totalAmounts.push(item.amount);
-                totalItems.push(item.title + " - € " + item.amount);
-            });
-            var chart = paper.piechart(150, 150, 100, totalAmounts, {
-                legend: totalItems, 
-                legendpos: "south", 
-            });  
-            var legendH = $("#raphael-container svg text").height() * items.length;
-            paper.setSize(paperW, paperH + legendH)
-        });
-    });
+    })
 }
 
 if (Meteor.isServer) {
